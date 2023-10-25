f5a10d1347c23e9cf309185d2ad4a2276ecc6716
Guilherme Barros <129431156+GuilhermeBarros862@users.noreply.github.com>
2023-10-25 15:43:23 -0300

    Update Empresa de Logistica.py

--- Empresa de Logistica.py
+++ Empresa de Logistica.py
@@ -1,17 +1,12 @@
 # Definição das funções
-
 def dimensoesObjeto():  # Definição de função para receber e calcular as dimensões para retornar um valor multiplicador
-
     while True:  # Loop para garantir que o usuário digitará corretamente
-
         try:  # Tratamento para se o usuário digitar corretamente
-
             comprimento = int(input('Digite o comprimento do objeto (em cm): '))
             largura = int(input('Digite a largura do objeto (em cm): '))
             altura = int(input('Digite a altura do objeto (em cm): '))  # Leitura dos valores para calcular a dimensão
 
             dimensao = comprimento * largura * altura  # Calculo das medidas recebidas, que, resulta na dimensão
-
             print(f'O volume do objeto é (em cm³) {dimensao:.1f}')  # Informa ao usuário a dimensão do objeto
 
             if dimensao < 1000:  # Verifica o volume da dimensão
@@ -30,17 +25,13 @@ def dimensoesObjeto():  # Definição de função para receber e calcular as dim
                 print('Não aceitamos objetos com dimensões tão grandes')
 
         except:  # Tratamento de erro para que se o usuário não digite um número inteiro
-
             print('Você digitou uma dimensão do objeto com valor não numérico')
             print('Por favor entre com as dimensões desejadas novamente')
 
 
 def pesoObjeto():  # Definição de função para receber o peso e retornar um multiplicador
-
     while True:
-
         try:
-
             peso = float(input('Digite o peso do objeto (em kg): '))
 
             if peso < 0.1:
@@ -62,9 +53,7 @@ def pesoObjeto():  # Definição de função para receber o peso e retornar um m
             print('Você digitou o peso do objeto com valor não numérico')
 
 def rotaObjeto():  # Definição de função para receber verificar a rota e retornar um multiplicador
-
     while True:
-
         print('Selecione a rota:')
         print('''
         BR - De Brasília para Rio de Janeiro
@@ -89,7 +78,6 @@ def rotaObjeto():  # Definição de função para receber verificar a rota e ret
             print('Você digitou uma rota que não existe')
 
 # Programa principal
-
 print("Bem Vindo a Companhia de Logística Guilherme Araújo de Barros S.A. (RU: 4390675)")
 
 multiplicadorDimensao = dimensoesObjeto()  # Chama a função e atribui seu retorno a variável
@@ -98,5 +86,5 @@ multiplicadorRota = rotaObjeto()
 
 total = multiplicadorDimensao * multiplicadorPeso * multiplicadorRota  # Calcula os multiplicadores recebidos
 
-print(f'Total a pagar (R$): {total:.2f} (dimensões: {multiplicadorDimensao:.1f} * peso: {multiplicadorPeso:.1f} * rota: {multiplicadorRota:.1f})')
 # Imprime o total a pagar e informa como foi feito o calculo
+print(f'Total a pagar (R$): {total:.2f} (dimensões: {multiplicadorDimensao:.1f} * peso: {multiplicadorPeso:.1f} * rota: {multiplicadorRota:.1f})')
